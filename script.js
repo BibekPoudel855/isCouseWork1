318623bdd7b5f908c45993bb6da3edb655b58124
Bibek Poudel <bibekpoudel855@gmail.com>
2025-01-09 09:01:07 +0545

    normal commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//selection of element
+shopNowButton= document.querySelector("#shop-now");
+console.log(shopNowButton);
+
+//shop now button function
+shopNowButton.addEventListener("click",()=>{
+    window.open("https://www.bibekpoudel.info.np");
+});
