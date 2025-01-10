faf0c38ba107ea215a967177cf14540c269de481
Bibek Poudel <bibekpoudel855@gmail.com>
2025-01-10 18:46:00 +0545

    added footer

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 //selection of element
-body=document.querySelector("body");
-shopNowButton= document.querySelector("#shop-now");
-moonForDarkMode=document.querySelector(".moon");
+body = document.querySelector("body");
+shopNowButton = document.querySelector("#shop-now");
+moonForDarkMode = document.querySelector("#dark-mode");
+lightForLightMode = document.querySelector("#light-mode");
 //dark mode functionalty
-let isLight=true;
-moonForDarkMode.addEventListener("click",()=>{
-    if(isLight){
-        body.style.backgroundColor="black";
-        body.style.color="white";
-        isLight=false;
-    }else{
-        body.style.backgroundColor="white";
-        body.style.color="black";
-        isLight=true;
-    }
-});
+let isLight = true;
+moonForDarkMode.addEventListener("click",modeChange);
+lightForLightMode.addEventListener("click",modeChange);
+function modeChange() {
+  if (isLight) {
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+    lightForLightMode.style.display = "block";
+    moonForDarkMode.style.display = "none";
+    isLight = false;
+  } else {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+    moonForDarkMode.style.display = "block";
+    lightForLightMode.style.display = "none";
+    isLight = true;
+  }
+}
 //shop now button function
-shopNowButton.addEventListener("click",()=>{
-    window.open("https://www.bibekpoudel.info.np");
+shopNowButton.addEventListener("click", () => {
+  window.open("https://www.bibekpoudel.info.np");
 });
