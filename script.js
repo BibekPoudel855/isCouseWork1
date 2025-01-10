0f2d4073bfa7e6123ad49fc809a49ea9bed12b6e
Bibek Poudel <bibekpoudel855@gmail.com>
2025-01-10 09:02:09 +0545

    added beta footer

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 //selection of element
+body=document.querySelector("body");
 shopNowButton= document.querySelector("#shop-now");
-console.log(shopNowButton);
-
+moonForDarkMode=document.querySelector(".moon");
+//dark mode functionalty
+let isLight=true;
+moonForDarkMode.addEventListener("click",()=>{
+    if(isLight){
+        body.style.backgroundColor="black";
+        body.style.color="white";
+        isLight=false;
+    }else{
+        body.style.backgroundColor="white";
+        body.style.color="black";
+        isLight=true;
+    }
+});
 //shop now button function
 shopNowButton.addEventListener("click",()=>{
     window.open("https://www.bibekpoudel.info.np");
