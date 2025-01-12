2f0d9ad31fa67c724b8efd7c0e38133fb06e3f45
Bibek Poudel <bibekpoudel855@gmail.com>
2025-01-12 21:09:32 +0545

    added meta tag and js for newslettter

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 //selection of element
-
+let shopNowButton = document.getElementById("shop-now");
+let newsLetterInput = document.getElementById("newsletter-input");
 //shop now button function
-function shopNowButtonClick(){
+shopNowButton.onclick = shopNowButtonEvent;
+function shopNowButtonEvent() {
   console.log("click");
+  open("https://www.bibekpoudel.info.np");
+}
+
+// buyNow Button
+let buyNowButton = () => {
   window.open("https://www.bibekpoudel.info.np");
+};
+
+//newsletter form validation
+function newsLetterValidation() {
+  var newsLetterInputValue = newsLetterInput.value;
+  if (newsLetterInputValue.length == "") {
+    alert("Input cant be Empty");
+  }else{
+    alert("thank you");
+  }
 }
