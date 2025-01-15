904775f77bd53d5d9d210bec089e363a4cf49c56
Bibek Poudel <bibekpoudel855@gmail.com>
2025-01-15 19:27:43 +0545

    chnages made in js in newsletter

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //selection of element
 let shopNowButton = document.getElementById("shop-now");
 let newsLetterInput = document.getElementById("newsletter-input");
+let newsLetterMessageP= document.getElementById("message-for-newsletter-validation");
+console.log(newsLetterMessageP);
 //shop now button function
 shopNowButton.onclick = shopNowButtonEvent;
 function shopNowButtonEvent() {
@@ -13,19 +15,21 @@ let buyNowButton = () => {
   window.open("https://www.bibekpoudel.info.np");
 };
 
-//newsletter form validation
+//newsletter input field validation
 function newsLetterValidation() {
   var newsLetterInputValue = newsLetterInput.value;
   console.log(newsLetterInputValue);
-
-  if ( newsLetterInputValue.trim() == " ") {
-    alert("Input cant be Empty");
+  if ( newsLetterInputValue == "") {
+    newsLetterMessageP.style.color="red";
+    newsLetterMessageP.innerText="Input cant be Empty";
+  } else if (newsLetterInputValue.length < 8) {
+    newsLetterMessageP.style.color="red";
+    newsLetterMessageP.innerText="Not valid Mail length";
+  } else if (!newsLetterInputValue.includes("@")) {
+    newsLetterMessageP.style.color="red";
+    newsLetterMessageP.innerText="Not valid Mail";
+  }else{
+    newsLetterMessageP.style.color="green";
+    newsLetterMessageP.innerText="Thank You For Subscribe";
   }
-  // } else if (newsLetterInputValue.length < 8) {
-  //   alert("Not valid Mail length");
-  // } else if (!newsLetterInputValue.includes("@")) {
-  //   alert("Not valid Mail");
-  // }else{
-  //   alert("thank you");
-  // }
 }
